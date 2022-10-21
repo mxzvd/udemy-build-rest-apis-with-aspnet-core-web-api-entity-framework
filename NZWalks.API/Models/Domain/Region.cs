14da6c3c3b69dc14dbc18ec65ad197779a8247f2
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-10-21 21:29:38 +0200

    Created domain models

---
+++ NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,15 @@
+namespace NZWalks.API.Models.Domain;
+
+public class Region
+{
+    public Guid Id { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public double Area { get; set; }
+    public double Lat { get; set; }
+    public double Long { get; set; }
+    public long Population { get; set; }
+
+    // Navigation Properties:
+    public IEnumerable<Walk> Walks { get; set; }
+}

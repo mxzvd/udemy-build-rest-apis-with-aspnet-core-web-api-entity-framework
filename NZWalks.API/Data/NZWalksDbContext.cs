94e11c52b20af9fb3dd4d7be8cf9c56dd9bb103a
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-10-21 21:47:08 +0200

    Created a DbContext and added it to the DI

---
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data;
+
+public class NZWalksDbContext : DbContext
+{
+    public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Region> Regions { get; set; }
+    public DbSet<Walk> Walks { get; set; }
+    public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+}

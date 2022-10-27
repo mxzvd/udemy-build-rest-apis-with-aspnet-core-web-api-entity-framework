ebcbbe58fbc79f1348099bbecef9ac2fadd02466
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-10-27 21:05:56 +0200

    Added validations for walk difficulty endpoints

--- NZWalks.API/Controllers/WalkDifficultiesController.cs
+++ NZWalks.API/Controllers/WalkDifficultiesController.cs
@@ -45,6 +45,12 @@ public class WalkDifficultiesController : Controller
     [HttpPost]
     public async Task<IActionResult> AddWalkDifficultyAsync(Models.DTO.AddWalkDifficultyRequest request)
     {
+        // Validate the incoming request:
+        if (!ValidateAddWalkDifficultyAsync(request))
+        {
+            return BadRequest(ModelState);
+        }
+
         // Convert DTO to Domain model:
         var walkDifficultyDomain = new Models.Domain.WalkDifficulty
         {
@@ -65,6 +71,12 @@ public class WalkDifficultiesController : Controller
     [Route("{id:guid}")]
     public async Task<IActionResult> UpdateWalkDifficultyAsync(Guid id, UpdateWalkDifficultyRequest request)
     {
+        // Validate the incoming request:
+        if (!ValidateUpdateWalkDifficultyAsync(request))
+        {
+            return BadRequest(ModelState);
+        }
+
         // Convert DTO to Domain model:
         var walkDifficultyDomain = new Models.Domain.WalkDifficulty
         {
@@ -101,4 +113,50 @@ public class WalkDifficultiesController : Controller
 
         return Ok(walkDifficultyDTO);
     }
+
+    #region Validations
+
+    private bool ValidateAddWalkDifficultyAsync(AddWalkDifficultyRequest request)
+    {
+        if (request == null)
+        {
+            ModelState.AddModelError(nameof(request), $"Request can not be empty.");
+            return false;
+        }
+
+        if (string.IsNullOrWhiteSpace(request.Code))
+        {
+            ModelState.AddModelError(nameof(request.Code), $"{nameof(request.Code)} must not be null or empty or whitespace.");
+        }
+
+        if (ModelState.ErrorCount > 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool ValidateUpdateWalkDifficultyAsync(UpdateWalkDifficultyRequest request)
+    {
+        if (request == null)
+        {
+            ModelState.AddModelError(nameof(request), $"Request can not be empty.");
+            return false;
+        }
+
+        if (string.IsNullOrWhiteSpace(request.Code))
+        {
+            ModelState.AddModelError(nameof(request.Code), $"{nameof(request.Code)} must not be null or empty or whitespace.");
+        }
+
+        if (ModelState.ErrorCount > 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    #endregion
 }
